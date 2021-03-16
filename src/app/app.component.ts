6289acf51eee71ac0cd6d0cc325fc43187012fff
Asger Schultz <asger.s@protonmail.com>
2021-03-16 18:51:07 +0100

    Kasper den Kæppede

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import raneellereske from "../assets/raneellereske.json";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,9 +11,35 @@ export class AppComponent {
   rane = "Rane";
   eske = "Eske";
 
-  get title(): string {
-    return Math.random() >= 0.5
-      ? `${this.rane} eller ${this.eske}`
-      : `${this.eske} eller ${this.rane}`;
+  orderRane: Array<String> = Array();
+  orderEske: Array<String> = Array();
+  order: Array<number>;
+  raneeske: Array<String>;
+  truth: Array<boolean> = Array();
+
+  i: number = 0;
+  title: string;
+  guessed = false;
+  correct = false;
+
+  constructor() {
+    this.title = Math.random() >= 0.5
+    ? `${this.rane} eller ${this.eske}`
+    : `${this.eske} eller ${this.rane}`;
+    this.order = [...Array(2 * raneellereske.eske.length).keys()];
+    this.order.sort((a, b) => Math.random() - 0.5);
+    this.raneeske = raneellereske["rane"];
+    this.raneeske.push(...raneellereske["eske"]);
+  }
+
+  public getImg(): String {
+    const name = this.order[this.i] >= raneellereske["eske"].length ? "eske" : "rane";
+    return `assets/imgs/${name}/${this.raneeske[this.order[this.i]]}`;
+  }
+
+  public guess(name: string) {
+    this.guessed = true;
+    this.correct = (this.order[this.i] >= raneellereske["eske"].length ? "eske" : "rane") === name;
+    this.i ++;
   }
 }
